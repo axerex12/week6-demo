cec7ed4ac4d493c338b1052157af1e4c25a5694b
axerex12 <axel.aaldijk12@gmail.com>
2025-09-25 13:40:09 +0300

    ass

--- src/main/java/TemperatureConverter.java
+++ src/main/java/TemperatureConverter.java
@@ -1,4 +1,4 @@
-public class TemperatureConverter {
+public class temperatureconverter {
 
     public static double fahrenheitToCelsius(double fahrenheit) {
         return (fahrenheit - 32) * 5 / 9;
